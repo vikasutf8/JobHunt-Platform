0b1be5ac39adbca604258500b8f05ced5481d343
vikasutf8 <21bcs241@iiitdmj.ac.in>
2024-10-30 15:28:44 +0530

    url updated

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOptions = {
-    origin:process.env.CLIENT_URL,
+    origin:"https://jobhunt-platform.onrender.com/",
     credentials:true
 }
 
