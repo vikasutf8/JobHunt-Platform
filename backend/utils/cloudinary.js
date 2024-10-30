381f4b316f009e0353339feb2581c7228d528915
vikasutf8 <21bcs241@iiitdmj.ac.in>
2024-10-30 14:28:57 +0530

    commit

--- backend/utils/cloudinary.js
+++ backend/utils/cloudinary.js
@@ -3,8 +3,8 @@ import dotenv from "dotenv";
 dotenv.config();
 
 cloudinary.config({
-    cloud_name:'dpit2cy8z',
-    api_key:184487417479196,
-    api_secret:'8yFseGTVFlnBv-iZPUtPobE5ld8'
+    cloud_name:process.env.CLOUNDINARY_NAME,
+    api_key:process.env.CLOUNDINARY_API_KEY,    
+    api_secret:process.env.CLOUDINARY_API_SECRET
 });
 export default cloudinary;
