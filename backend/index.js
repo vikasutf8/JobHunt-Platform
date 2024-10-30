88aaeccf8aae9b6458b53f2d77391108977661f7
vikasutf8 <21bcs241@iiitdmj.ac.in>
2024-10-30 15:19:35 +0530

    prodcutions

--- backend/index.js
+++ backend/index.js
@@ -7,11 +7,11 @@ import userRoute from "./routes/user.route.js";
 import companyRoute from "./routes/company.route.js";
 import jobRoute from "./routes/job.route.js";
 import applicationRoute from "./routes/application.route.js";
-
+import path from "path";
 dotenv.config({});
 
 const app = express();
-
+const __dirname = path.resolve();
 // middleware
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
@@ -33,7 +33,10 @@ app.use("/api/v1/job", jobRoute);
 app.use("/api/v1/application", applicationRoute);
 
 
-
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 app.listen(PORT,()=>{
     connectDB();
     console.log(`Server running at port ${PORT}`);
